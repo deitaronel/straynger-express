15de536286b11deebba5db3ec81ea6e1d66717f6
roneldeita <ronel.deita@beehive.hk>
2024-06-16 03:32:50 +0800

    add ssl config to pg connection

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const db = new pg.Client({
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT
+  port: process.env.PG_PORT,
+  ssl: process.env.PG_SSL == 0 ? false : true
 });
 
 db.connect();
