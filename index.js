10f02de8aa8435bc07730fed1c71768e9949e7b1
roneldeita <ronel.deita@beehive.hk>
2024-06-16 23:49:24 +0800

    update pg connection

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import env from "dotenv";
-import pg from "pg";
+import pkg from "pg";
 
 const app = express();
 const port = 3000;
 env.config();
 
-
-const db = new pg.Client({
+const { Pool } = pkg;
+const db = new Pool({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DB,
