8535807f1ea91a61c1888370e303be6c33217096
roneldeita <ronel.deita@beehive.hk>
2024-06-18 19:50:48 +0800

    commented out db connection for aws testing

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 import express from "express";
 import http from "http";
 import bodyParser from "body-parser";
-import env from "dotenv";
-import pg from "pg";
+// import env from "dotenv";
+// import pg from "pg";
 
 const app = express();
 const port = 3000;
-env.config();
+// env.config();
 
-const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DB,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-});
+// const db = new pg.Client({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DB,
+//   password: process.env.PG_PASSWORD,
+//   port: process.env.PG_PORT,
+// });
 
-db.connect();
+// db.connect();
 
   // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/users", async (req, res) => {
-    try{
-      const users = await db.query("SELECT * from users");
-      if(users.rows.length > 0){
-        res.status(200).json(users);
-      }else{
-        res.status(404).json({message: "No Users Found"});
-      }
-    }catch(err){
-      console.log(err)
-    }
-});
+// app.get("/users", async (req, res) => {
+//     try{
+//       const users = await db.query("SELECT * from users");
+//       if(users.rows.length > 0){
+//         res.status(200).json(users);
+//       }else{
+//         res.status(404).json({message: "No Users Found"});
+//       }
+//     }catch(err){
+//       console.log(err)
+//     }
+// });
   
 app.get("/", (req, res) => {
     res.send("Hello World!!")
