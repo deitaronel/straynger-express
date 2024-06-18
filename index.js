2776f47a566dab0593a88e37b65827a0eaa06fe4
roneldeita <ronel.deita@beehive.hk>
2024-06-18 15:45:29 +0800

    restore db connection

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 import express from "express";
+import http from "http";
 import bodyParser from "body-parser";
+import env from "dotenv";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+env.config();
 
-// Middleware
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+db.connect();
+
+  // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get("/users", async (req, res) => {
+    try{
+      const users = await db.query("SELECT * from users");
+      if(users.rows.length > 0){
+        res.status(200).json(users);
+      }else{
+        res.status(404).json({message: "No Users Found"});
+      }
+    }catch(err){
+      console.log(err)
+    }
 });
+  
+app.get("/", (req, res) => {
+    res.send("Hello World!!")
+});
+
+const httpServer = http.createServer(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
